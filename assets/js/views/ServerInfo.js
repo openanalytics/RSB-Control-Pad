26cbe9c08d3c7c5fcceeb2d0d77c1c367c9c48ac
David Dossot <david@dossot.net>
2011-11-15 13:30:08 -0800

    fixed tab layout of server info card

--- assets/js/views/ServerInfo.js
+++ assets/js/views/ServerInfo.js
@@ -23,117 +23,140 @@
 
 // TODO add refresh button to update server info when clicked
  
-app.views.ServerInfo = Ext.extend(Ext.form.FormPanel, {
+app.views.ServerInfo = Ext.extend(Ext.TabPanel, {
   dockedItems: [
-  {
-    xtype: 'toolbar',
-    dock: 'top',
-    title: 'RSB Server',
-    items: [
-      {
-        id: 'back',
-        text: 'Back',
-        ui: 'back',
-        listeners: {
-          'tap': function () {
-            Ext.dispatch({
-              controller: app.controllers.servers,
-              action: 'list',
-              animation: {type:'slide', direction:'right'}
-            });
+    {
+      xtype: 'toolbar',
+      dock: 'top',
+      title: 'RSB Server',
+      items: [
+        {
+          id: 'back',
+          text: 'Back',
+          ui: 'back',
+          listeners: {
+            'tap': function () {
+              Ext.dispatch({
+                controller: app.controllers.servers,
+                action: 'list',
+                animation: {type:'slide', direction:'right'}
+              });
+            }
+          }
+        },
+        {xtype:'spacer'},
+        {
+          id: 'edit',
+          text: 'Edit',
+          ui: 'edit',
+          listeners: {
+            'tap': function () {
+              // TODO implement editing + deleting existing server
+              Ext.Msg.alert('Sorry!', 'Editing is not yet implemented.', Ext.emptyFn);
+            }
           }
         }
+      ]
+    },
+    {
+      xtype: 'toolbar',
+      dock: 'bottom',
+      defaults: {
+        iconMask: true,
+        ui: 'plain'
       },
-      {xtype:'spacer'},
-      {
-        id: 'edit',
-        text: 'Edit',
-        ui: 'edit',
-        listeners: {
-          'tap': function () {
-            // TODO implement editing + deleting existing server
-            Ext.Msg.alert('Sorry!', 'Editing is not yet implemented.', Ext.emptyFn);
+      layout: {
+        pack: 'center'
+      },
+      items: [
+        {
+          id: 'refresh',
+          iconCls: 'refresh',
+          title: 'Refresh',
+          listeners: {
+            'tap': function () {
+              var record = this.record;
+              
+              fetchServerInfo(record.data.url, record.data.username, record.data.password,
+                function(nodeInformation, status) {
+                  record.set({status: status,
+                              node_information: nodeInformation,
+                              last_updated: new Date()});
+                  record.save();
+                  app.views.serverInfo.updateWithRecord(record);
+                }
+              );
+            }
           }
         }
-      }
-    ]
-  },
-  {
-    xtype: 'toolbar',
+      ]
+    }
+  ],
+  tabBar: {
     dock: 'bottom',
-    defaults: {
-      iconMask: true,
-      ui: 'plain'
+    scroll: {
+      direction: 'horizontal',
+      useIndicators: false
     },
     layout: {
       pack: 'center'
-    },
-    items: [
-      {
-        id: 'refresh',
-        iconCls: 'refresh',
-        title: 'Refresh',
-        listeners: {
-          'tap': function () {
-            var record = this.record;
-            
-            fetchServerInfo(record.data.url, record.data.username, record.data.password,
-              function(nodeInformation, status) {
-                record.set({status: status,
-                            node_information: nodeInformation,
-                            last_updated: new Date()});
-                record.save();
-                app.views.serverInfo.updateWithRecord(record);
-              }
-            );
-          }
-        }
-      }
-    ]
-  }
-  ],
-  scroll: 'vertical',
-  submitOnAction: false,
-  styleHtmlContent:true,
-  instructions: 'TODO change this to last updated?',
+    }
+  },
+  layout: 'fit',
   items: [
-    {tpl:[
-      '<h4>Settings</h4>',
-      '<div class="field"><span class="label">URL: </span>{url}</div>',
-      // TODO display a lock if server is username/password secured
-    ]},
-    {tpl:[
-      '<h4>Information</h4>',
-      '<div class="field"><span class="label">Name:</span>{node_information.name}</div>',
-      '<div class="field"><span class="label">Healthy:</span>{node_information.healthy}</div>',
-      '<div class="field"><span class="label">Uptime:</span>{node_information.uptimeText}</div>',
-      '<div class="field"><span class="label">Container:</span>{node_information.servletContainerInfo}</div>',
-    ]},
-    {tpl:[
-      '<h4>JVM Memory</h4>',
-      '<div class="field"><span class="label">Free:</span>{node_information.jvmFreeMemory}</div>',
-      '<div class="field"><span class="label">Max:</span>{node_information.jvmMaxMemory}</div>',
-    ]},
-    {tpl:[
-      '<h4>OS</h4>',
-      '<div class="field"><span class="label">Load avg:</span>{node_information.osLoadAverage}</div>',
-    ]},
-    {tpl:[
-      '<h4>Applications</h4>',
-      '<div class="field">TBD...</div>'
-    ]},
-    {tpl:[
-      '<div class="footnote">Last updated: {last_updated}</div>'
-    ]}
+    {
+      iconCls: 'info',
+      title: 'Info',
+      items: {
+        xtype: 'formpanel',
+        scroll: 'vertical',
+        styleHtmlContent:true,
+        submitOnAction: false,
+        items: [ 
+          {tpl:[
+            '<h4>Settings</h4>',
+            '<div class="field"><span class="label">URL: </span>{url}</div>'
+            // TODO display a lock if server is username/password secured
+          ]},
+          {tpl:[
+            '<h4>Information</h4>',
+            '<div class="field"><span class="label">Name:</span>{node_information.name}</div>',
+            '<div class="field"><span class="label">Healthy:</span>{node_information.healthy}</div>',
+            '<div class="field"><span class="label">Uptime:</span>{node_information.uptimeText}</div>',
+            '<div class="field"><span class="label">Container:</span>{node_information.servletContainerInfo}</div>',
+          ]},
+          {tpl:[
+            '<h4>JVM Memory</h4>',
+            '<div class="field"><span class="label">Free:</span>{node_information.jvmFreeMemory}</div>',
+            '<div class="field"><span class="label">Max:</span>{node_information.jvmMaxMemory}</div>',
+          ]},
+          {tpl:[
+            '<h4>OS</h4>',
+            '<div class="field"><span class="label">Load avg:</span>{node_information.osLoadAverage}</div>',
+          ]},
+          {tpl:[
+            '<h4>Applications</h4>',
+            '<div class="field">TBD...</div>'
+          ]},
+          {tpl:[
+            '<div class="footnote">Last updated: {last_updated}</div>'
+          ]}
+        ]
+      }
+    }
   ],
   initComponent: function() {
-    // TODO add manual refresh
     // TODO add automatic refresh
     app.views.ServerInfo.superclass.initComponent.apply(this, arguments);
   },
   updateWithRecord: function(record) {
+    // take a deep items dive :)
     Ext.each(this.items.items, function(item) {
-      item.update(record.data);
+      item.items.each(function(item2) {
+        item2.items.each(function(item3) {
+          item3.update(record.data);
+        });
+      });
     });
     var toolbar = this.getDockedItems()[0];
     toolbar.getComponent('edit').record = record;
